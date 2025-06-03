54ee4acf5641fa77573b0151df56af9c95759bf5
ngyimesi <ngyimesi@sch.bme.hu>
2025-06-03 12:40:11 +0200

    Ignored node-modules

--- initialize.sh
+++ initialize.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 DNF_CMD=$(which dnf)
 APT_GET_CMD=$(which apt-get)
-if [[ ! -z $YUM_CMD ]]; then
-    sudo dnf install -y ansible ansible-galaxy python3-passlib
+if [[ ! -z $DNF_CMD ]]; then
+    sudo dnf install -y ansible ansible-galaxy python3-passlib vagrant
 elif [[ ! -z $APT_GET_CMD ]]; then
     sudo apt update
     sudo apt install -y ansible ansible-galaxy python3-passlib
 fi
-
+vagrant plugin install vagrant-libvirt
